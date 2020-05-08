7b0fa664ab3b819570bd29aa5b652ba7821e852c
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-05-08 00:02:14 -0600

    mvp

--- practice_app/config/routes.rb
+++ practice_app/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :tasks
-  resources :users
+  # - Limit users resource to *only* allow `index` and `show`
+  # - Limit tasks resource to allow everything *except* `destroy`
+  # - remove the corresponding controller actions that are no longer routable
+  resources :tasks, :only => [:index, :show, :edit, :create, :update, :new]
+  # resources :users
+  resources :users, :only => [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
